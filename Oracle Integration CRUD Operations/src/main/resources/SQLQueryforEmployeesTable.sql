fd4b276b549ddb7688640b5f68bbdeecf4d28acc
Harsh Chhajer <harshprashantchhajer@gmail.com>
2024-10-03 16:55:01 +0530

    Update SQLQueryforEmployeesTable.sql

--- Oracle Integration CRUD Operations/src/main/resources/SQLQueryforEmployeesTable.sql
+++ Oracle Integration CRUD Operations/src/main/resources/SQLQueryforEmployeesTable.sql
@@ -11,19 +11,19 @@ END;
 CREATE TABLE Employee101 (
                              Fname VARCHAR2(50),
                              Lname VARCHAR2(50),
-                             Bdate DATE
+                             Bdate VARCHAR2(20)
 );
 
 INSERT INTO employee101 (Fname, Lname, Bdate)
-VALUES ('Perseus', 'Danae',TO_DATE('2000-02-13', 'YYYY-MM-DD'));
+VALUES ('Perseus', 'Danae',TO_DATE('2000-02-13'));
 
 INSERT INTO employee101 (Fname, Lname, Bdate)
-VALUES ('Athena', 'Parthenos', TO_DATE('1995-05-24', 'YYYY-MM-DD'));
+VALUES ('Athena', 'Parthenos', TO_DATE('1995-05-24'));
 
 INSERT INTO employee101 (Fname, Lname, Bdate)
-VALUES ('Heracles', 'Alcides', TO_DATE('1987-07-16', 'YYYY-MM-DD'));
+VALUES ('Heracles', 'Alcides', TO_DATE('1987-07-16'));
 
 INSERT INTO employee101 (Fname, Lname, Bdate)
-VALUES ('Andromeda', 'Cassiopeia', TO_DATE('2001-09-30', 'YYYY-MM-DD'));
+VALUES ('Andromeda', 'Cassiopeia', TO_DATE('2001-09-30'));
 
 Select * From employee101;
